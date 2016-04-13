683a2394fa3e8b439b59090ceff124b97cffdffb
Eric Nelson <ericn@berkeley.edu>
2016-04-13 22:22:40 +0000

    Got rid of avails

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160414201115) do
+
+ActiveRecord::Schema.define(version: 20160413223013) do
 
   create_table "avails", force: :cascade do |t|
     t.integer  "day"

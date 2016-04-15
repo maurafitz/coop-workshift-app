9dd7983bfa2a458567ca9a0508f37b38b353b3c0
Eric Nelson <ericn@berkeley.edu>
2016-04-15 20:13:23 +0000

    Try again

--- features/support/env.rb
+++ features/support/env.rb
@@ -25,6 +25,7 @@ require "codeclimate-test-reporter"
 
 require 'capybara/poltergeist'
 Capybara.javascript_driver = :poltergeist
+
 ## IGNORE ERRORS
 Capybara.register_driver :poltergeist do |app|
   Capybara::Poltergeist::Driver.new(app, {js_errors: false})

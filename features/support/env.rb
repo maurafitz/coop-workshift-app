512106e0e2280dcc32b596a0d24814e64ea40ad8
Eric Nelson <ericn@berkeley.edu>
2016-04-15 20:03:36 +0000

    ignore JS erros (for now)

--- features/support/env.rb
+++ features/support/env.rb
@@ -26,9 +26,9 @@ require "codeclimate-test-reporter"
 require 'capybara/poltergeist'
 Capybara.javascript_driver = :poltergeist
 ## IGNORE ERRORS
-# Capybara.register_driver :poltergeist do |app|
-  # Capybara::Poltergeist::Driver.new(app, {js_errors: false})
-# end
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, {js_errors: false})
+end
 
 # Capybara defaults to CSS3 selectors rather than XPath.
 # If you'd prefer to use XPath, just uncomment this line and adjust any

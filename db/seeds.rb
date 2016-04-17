6e523fd4fe1a906270ff339b5efa0e86ba210d29
Giorgia Willits <gwillits@berkeley.edu>
2016-04-17 19:59:32 +0000

    changed seed delete order

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
-Unit.destroy_all
-Policy.destroy_all
 Metashift.destroy_all
-Shift.destroy_all
 User.destroy_all
+Policy.destroy_all
+Shift.destroy_all
+Unit.destroy_all
 
 
 ## POLICIES ## 

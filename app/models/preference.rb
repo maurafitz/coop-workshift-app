a1f39bfb55b196ddc233a807cdc75b26ce237ee1
Giorgia Willits <gwillits@berkeley.edu>
2016-04-20 22:06:36 +0000

    started moving to preference model

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -1,4 +1,27 @@
 class Preference < ActiveRecord::Base
   belongs_to :user
   belongs_to :metashift
+  
+  DEFAULT_RANK = 3
+  
+  def set_rankings cat_rank, ms_rank
+    if cat_rank != 0 and ms_rank == 0
+      self.rating = cat_rating
+    else
+      self.rating = ms_rank
+    end
+    self.cat_rating = cat_rank
+  end
+  
+  def get_ranking
+    if self.ranking != 0
+      self.ranking
+    else
+      DEFAULT_RANK
+    end
+  end
+  
+  def get_raw_ranking
+    
+  end
 end

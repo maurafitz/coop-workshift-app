16d4777d707210841c903fafdd3dc73b761ff98f
Giorgia Willits <gwillits@berkeley.edu>
2016-04-25 03:30:06 +0000

    migrations pending

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160424223804) do
+ActiveRecord::Schema.define(version: 20160425015458) do
 
   create_table "avails", force: :cascade do |t|
     t.integer  "day"

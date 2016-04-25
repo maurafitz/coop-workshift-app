d6b8448cac3adb3a1d6277dcdf39b50b306df258
adanilychevjr <adanilychevjr@gmail.com>
2016-04-25 21:57:32 +0000

    Passing shifts w/ nested info correctly to view

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -4,6 +4,7 @@ class Shift < ActiveRecord::Base
   belongs_to :signoff_by, :class_name => "User"
   
   def full_json
-    self.as_json( include: [:workshift, :user] )
+    self.as_json(:include=> {:workshift=> {:include=> :metashift}, 
+    :user => {}} )
   end
 end

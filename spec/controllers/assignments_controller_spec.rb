0a65e356ca05f40ba3714c68d043e98c2311b173
Giorgia Willits <gwillits@berkeley.edu>
2016-04-29 22:06:34 +0000

    assignments_controller part 1

--- spec/controllers/assignments_controller_spec.rb
+++ spec/controllers/assignments_controller_spec.rb
@@ -48,22 +48,5 @@ RSpec.describe AssignmentsController, type: :controller do
         end
         context 'when the current user is a member ' do 
         end
-        
-        
-        # before(:each) do
-        #     @a_user = User.create!(:first_name => 'my user', :last_name => 'last',
-        #     :email => 'auser@gmail.com', :password => '3ljkd;a2', :permissions =>
-        #     User::PERMISSION[:ws_manager])
-        #     request.session = { :user_id => @a_user.id }
-        # end
-        # it 'should call the assign method' do
-        #     expect_any_instance_of(AssignmentsController).to receive(:assign_workshifts)
-        #     post :create 
-        # end
-        # it 'should redirect to the user profile' do
-        #     AssignmentsController.any_instance.stub(:assign_workshifts)
-        #     post :create
-        #     expect(response).to redirect_to("/users/#{@a_user.id}")
-        # end
     end
 end

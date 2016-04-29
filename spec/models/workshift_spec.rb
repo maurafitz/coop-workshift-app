d363d42c30b82bb0bc30430851b1eb6177ffbf3d
Yannie Yip <yannie.yip@berkeley.edu>
2016-04-29 22:59:44 +0000

    workshift spec return correct time format

--- spec/models/workshift_spec.rb
+++ spec/models/workshift_spec.rb
@@ -6,7 +6,13 @@ RSpec.describe Workshift, type: :model do
         should have_many(:shifts)
         should belong_to(:metashift)
     }
-    # before :each do
-        # @workshift = Workshift.new(:start_time => start_time, :end_time => end_time, :day => day, :length => length)
-    # end
+    
+    before :each do
+        @workshift = Workshift.new(:start_time => "10am", :end_time => "1pm", :day => "Monday", :length => "3")
+    end
+    
+    it "should return the correct formatted time do" do
+        expect(@workshift.get_time_formatted).to eq("Monday, 10am to 1pm")
+    end
 end
+    

871ee1ab1ca554a33c9a4669908183a12a4faf88
Eric Nelson <ericn@berkeley.edu>
2016-04-29 00:53:31 +0000

    Trying to fix heroku

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -28,8 +28,12 @@ class AssignmentsController < ApplicationController
   end
   
   def sort_users
+    puts params
     workshift = Workshift.find_by_id(params[:id])
+    puts workshift
+    puts current_unit
     @sorted_users_rankings = User.get_rankings_for workshift, current_unit
+    puts @sorted_users_rankings
     @rows, @names, @mapping= [], [], {}
     @sorted_users_rankings.each do |user, ranking|
       @rows << {:name => "<a id='#{user.last_name}' href='#{admin_view_user_path(user.id)}'>#{user.full_name}</a>", :ranking => ranking}

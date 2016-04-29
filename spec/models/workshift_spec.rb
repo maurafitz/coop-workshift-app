8ae4fdde316c9f8a262ba6a4d449639aae2bb3d5
Yannie Yip <yannie.yip@berkeley.edu>
2016-04-29 22:52:45 +0000

    workshift_spec

--- spec/models/workshift_spec.rb
+++ spec/models/workshift_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Workshift, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    it { 
+        should belong_to(:user) 
+        should have_many(:shifts)
+        should belong_to(:metashift)
+    }
+    # before :each do
+        # @workshift = Workshift.new(:start_time => start_time, :end_time => end_time, :day => day, :length => length)
+    # end
 end

1d25c1a32bc2a4b5d11ba67b9bbe8c567e7c1261
Giorgia Willits <gwillits@berkeley.edu>
2016-04-29 23:45:24 +0000

    assignments part 2

--- spec/controllers/assignments_controller_spec.rb
+++ spec/controllers/assignments_controller_spec.rb
@@ -1,11 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe AssignmentsController, type: :controller do
+    before :each do
+        @unit = double("cloyne")
+        @admin = double("Admin", :unit => @unit, :is_ws_manager? => true, :id => "0")
+        allow(User).to receive(:find_by_id).and_return(@admin)
+    end
     describe 'creating workshift assignments' do
         context 'when the current user is an admin' do
             before :each do
-                @unit = double("Cloyne")
-                @admin = double("Admin", :unit => @unit, :is_ws_manager? => true, :id => "0")
                 @metashift_rows = double("Metashifts")
                 @workshift1, @workshift2, @workshift3 = double("Workshift1"), double("Workshift2"), double("Workshift3")
                 @user1, @user2 = double("User1"), double("User2")
@@ -13,7 +16,6 @@ RSpec.describe AssignmentsController, type: :controller do
                 @users = {"1" => @user1, "2" => @user2}
                 @params = {"workshifts" => {"1"=> "1", "2" => "2", "3" => "1"}}
                 
-                allow(User).to receive(:find_by_id).and_return(@admin)
                 allow(@unit).to receive(:get_metashift_workshifts).and_return(@metashift_rows)
                 allow(Workshift).to receive(:find_by_id) do |id|
                     @workshifts[id]
@@ -48,5 +50,41 @@ RSpec.describe AssignmentsController, type: :controller do
         end
         context 'when the current user is a member ' do 
         end
+    end
+    describe 'providing json for the workshift' do
+        before :each do
+            @workshift = double("workshift", :id => 0)
+            @params = {"id" => @workshift.id}
+            @user1 = double("user1", :last_name => 'Willits', :id => 1, :full_name => "Giorgia Willits")
+            @user2 = double("user2", :last_name => 'Nelson', :id => 3, :full_name => "Eric Nelson")
+            @sorted_users_rankings = {@user1 => 4, @user2 => 1}
+            allow(Workshift).to receive(:find_by_id).and_return(@workshift)
+            allow(User).to receive(:get_rankings_for).and_return(@sorted_users_rankings)
+        end
+        it 'should get the selected workshift' do 
+            expect(Workshift).to receive(:find_by_id).with(@workshift.id.to_s)
+            get :sort_users, @params
+            expect(assigns(:workshift)).to eq(@workshift)
+        end
+        it 'should get all available users rankings for that workshift' do
+            expect(User).to receive(:get_rankings_for).with(@workshift)
+            get :sort_users, @params
+            expect(assigns(:sorted_users_rankings)).to eq(@sorted_users_rankings)
+        end
+        it 'should create a row for each available user' do
+            @rows = []
+            @sorted_users_rankings.each do |user, ranking|
+                expect(user).to receive(:last_name)
+                expect(user).to receive(:id)
+                expect(user).to receive(:full_name)
+                @rows << {:name => "<a id='#{user.last_name}' href='#{admin_view_user_path(user.id)}'>#{user.full_name}</a>", :ranking => ranking}
+            end
+            get :sort_users, @params
+            expect(assigns(:rows)).to eq(@rows)
+        end
+        # it 'should render json with the table rows' do
+            # get :sort_users, @params
+            # expect(response).to render_json
+        # end
     end
 end

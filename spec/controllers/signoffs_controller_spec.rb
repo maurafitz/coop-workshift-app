c35a0f57b73f69672f73b7dc909d115a8affcbeb
momochanfitz <momochanfitz@gmail.com>
2016-04-29 04:24:34 +0000

    try travis again

---
+++ spec/controllers/signoffs_controller_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe SignoffsController, type: :controller do
+    before(:each) do 
+       
+    end
+    describe 'configuring the unit' do
+        it 'should successfully save a valid unit' do
+        end
+        it 'should error if an invalid unit is provided' do
+        end
+    end
+end
